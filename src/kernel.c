9beab17b3df9820eabd19e432dd3463da7626e0c
sigmawq <longlance21@gmail.com>
2021-11-17 10:06:47 +0200

    Colored text

--- src/kernel.c
+++ src/kernel.c
@@ -55,26 +55,19 @@ static const size_t VGA_HEIGHT = 25;
  
 size_t terminal_row;
 size_t terminal_column;
-uint8_t terminal_color;
 uint16_t* terminal_buffer;
  
 void terminal_initialize(void) 
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
+			terminal_buffer[index] = vga_entry(' ', vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
 		}
 	}
-}
- 
-void terminal_setcolor(uint8_t color) 
-{
-	terminal_color = color;
 }
  
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
@@ -83,14 +76,15 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = vga_entry(c, color);
 }
  
-void terminal_putchar(char c) 
+void terminal_putchar(char c, enum vga_color fg, enum vga_color bg) 
 {
     if (c == '\n') {
         terminal_column = 0;
         terminal_row += 1;
         return;
     }
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    uint8_t color = vga_entry_color(fg, bg);
+	terminal_putentryat(c, color, terminal_column, terminal_row);
     
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -99,15 +93,26 @@ void terminal_putchar(char c)
 	}
 }
  
-void terminal_write(const char* data, size_t size) 
-{
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
-}
- 
 void terminal_writestring(const char* data) 
 {
-	terminal_write(data, strlen(data));
+    int size = strlen(data);
+    for (size_t i = 0; i < size; i++) {
+        terminal_putchar(data[i], VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+    }
+}   
+
+void colored_text(const char *data) {
+   int size = strlen(data);
+   for (size_t i = 0; i < size; i++) {
+        terminal_putchar(data[i], i % 15, VGA_COLOR_BLACK);
+   }
+}
+
+void more_colored_text(const char *data) {
+   int size = strlen(data);
+   for (size_t i = 0; i < size; i++) {
+        terminal_putchar(data[i], i % 15, (i % 2) + 2);
+   }
 }
  
 void kernel_main(void) 
@@ -116,5 +121,8 @@ void kernel_main(void)
 	terminal_initialize();
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\nHello New Line");
+	terminal_writestring("Hello, kernel World!\nHello New Line!\n");
+    colored_text("Hello colored text!\n");
+    more_colored_text("Hello more colored text!\n");
+
 }
